ed18deb0becb7321cb2183208b095e27dad95818
Koushika <koushika.sekar@wipro.com>
2022-01-12 20:33:29 +0530

    First Commit

---
+++ src/main/java/com/wipro/EmpRepository.java
@@ -0,0 +1,10 @@
+package com.wipro;
+
+//import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmpRepository extends CrudRepository<Employee, Integer>{
+
+}
--- src/main/java/com/wipro/Employee.java
+++ src/main/java/com/wipro/Employee.java
@@ -1,39 +1,77 @@
 package com.wipro;
 
+//import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+//import javax.persistence.Table;
+
+
+//import com.org.Organization;
+
+@Entity
+// @Table
 public class Employee {
+	@Id
+	@GeneratedValue
+//	@Column  
+	private int id;
+//	@Column  
 	private String name;
+//	@Column  
 	private int age;
-	private int salary;
-	 
-	public Employee(String name,int age,int salary) {
-	super();
-	this.name=name;
-	this.age=age;
-	this.salary=salary;
+//	@Column  
+	private long salary;
+//	@Column  
+	private String orgName;
+	
+	public Employee() {
+		
 	}
 	
+	public Employee(int id, String name, int age, long salary,String orgName ) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.salary = salary;
+		this.orgName = orgName;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-	
 	public int getAge() {
 		return age;
 	}
-
 	public void setAge(int age) {
 		this.age = age;
 	}
-
-	public int getSalary() {
+	public long getSalary() {
 		return salary;
 	}
-
-	public void setSalary(int salary) {
+	public void setSalary(long salary) {
 		this.salary = salary;
 	}
 
+	public String getOrgName() {
+		return orgName;
+	}
+
+	public void setOrgName(String orgName) {
+		this.orgName = orgName;
+	}
+	 
+	
+	
 }
